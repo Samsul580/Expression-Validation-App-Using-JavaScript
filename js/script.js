a93b7499c98db61a4a2b289dc69b54070beeab16
samsul580 <samsulshekh2019@gmail.com>
2023-06-04 14:49:42 +0600

    chore: add mobile phone validation and review the js file code

--- js/script.js
+++ js/script.js
@@ -4,13 +4,33 @@ let postCodeBtn = document.getElementById("post_code_btn");
 
 let resContainer = document.getElementById("res_container");
 
+// Phone number validation check function
+phoneBtn.addEventListener('click',function () {
+    let re = /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/;
+    let user_input = prompt("Input phone number");
+    let res = re.test(user_input);
+    if (user_input == null || user_input == "") {
+        alert("Please input a phone number!");
+    } else {
+        switch (res) {
+            case true:
+                resContainer.innerText = "Phone number is Valid";
+                break;
+        
+            default:
+                resContainer.innerText = "Phone number is Invalid";
+                break;
+        }
+    }
+})
+
 // Email validation check function
 emailBtn.addEventListener('click', function () {
     let re = /^([a-zA-Z0-9].?)+[^.]@([a-zA-Z0-9].?)+[^.]$/ ;
-    let user_input = prompt("Input Email Address")
-    let res = re.test(user_input)
+    let user_input = prompt("Input Email Address");
+    let res = re.test(user_input);
     if (user_input == null || user_input == "") {
-        alert("Please input a email address!")
+        alert("Please input a email address!");
     } else {
         switch (res) {
             case true:
@@ -18,7 +38,7 @@ emailBtn.addEventListener('click', function () {
                 break;
         
             default:
-                resContainer.innerText = "Post Address is Invalid";
+                resContainer.innerText = "Email Address is Invalid";
                 break;
         }
     }
@@ -27,11 +47,11 @@ emailBtn.addEventListener('click', function () {
 // Post Code validation check function
 postCodeBtn.addEventListener('click', function () {
     let re = /^[0-9]{4}$/;
-    let user_input = prompt("Input Post Code!")
-    let res = re.test(user_input)
+    let user_input = prompt("Input Post Code!");
+    let res = re.test(user_input);
 
     if (user_input == null || user_input == "") {
-        alert("Please input a post code!")
+        alert("Please input a post code!");
     } else {
         switch (res) {
             case true:
@@ -44,24 +64,3 @@ postCodeBtn.addEventListener('click', function () {
         }
     }
 })
-
-// Examples
-// let re;
-// let str;
-
-// Postal Code
-// 4700, 4000
-// re = /^[0-9]{4}$/;
-// str = "40000";
-
-// Phone Number
-// 01717888888 +8801717888888 8801717888888
-// re = /^(+)?(88)?01[0-9]{9}$/;
-// str = "+8801717888888";
-
-// Email Address
-// bohubrihi8.learn@edu.com.bd
-// re = /^([a-zA-Z0-9].?)+[^.]@([a-zA-Z0-9].?)+[^.]$/ ;
-// str = "bohubrihi.learn@edu.com.bd" ;
-
-// console.log(re.test(str)); 
